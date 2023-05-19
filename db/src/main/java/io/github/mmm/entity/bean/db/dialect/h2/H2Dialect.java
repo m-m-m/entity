51f2d95514610d941c59f5c3a4ff84df1c96030c
Jörg Hohwiller <hohwille@users.sourceforge.net>
2023-05-19 12:33:15 +0200

    added missing headers

--- db/src/main/java/io/github/mmm/entity/bean/db/dialect/h2/H2Dialect.java
+++ db/src/main/java/io/github/mmm/entity/bean/db/dialect/h2/H2Dialect.java
@@ -1,3 +1,5 @@
+/* Copyright (c) The m-m-m Team, Licensed under the Apache License, Version 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0 */
 package io.github.mmm.entity.bean.db.dialect.h2;
 
 import io.github.mmm.entity.bean.db.dialect.AbstractDbDialect;
