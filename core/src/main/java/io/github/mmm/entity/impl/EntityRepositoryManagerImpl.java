1b6237a2db8d7a06a45abc8efa7133b55536a675
Jörg Hohwiller <joerg.hohwiller@capgemini.com>
2023-12-19 19:24:04 +0100

    ServiceHelper refactoring

--- core/src/main/java/io/github/mmm/entity/impl/EntityRepositoryManagerImpl.java
+++ core/src/main/java/io/github/mmm/entity/impl/EntityRepositoryManagerImpl.java
@@ -9,6 +9,7 @@ import java.util.ServiceLoader;
 
 import io.github.mmm.base.collection.ReadOnlyIterator;
 import io.github.mmm.base.exception.ObjectNotFoundException;
+import io.github.mmm.base.service.ServiceHelper;
 import io.github.mmm.entity.Entity;
 import io.github.mmm.entity.repository.EntityRepository;
 import io.github.mmm.entity.repository.EntityRepositoryManager;
@@ -18,40 +19,35 @@ import io.github.mmm.entity.repository.EntityRepositoryManager;
  *
  * @since 1.0.0
  */
-@SuppressWarnings("rawtypes")
 public class EntityRepositoryManagerImpl implements EntityRepositoryManager {
 
   /** The singleton instance. */
   public static final EntityRepositoryManagerImpl INSTANCE = new EntityRepositoryManagerImpl();
 
-  private final Map<Class, EntityRepository<?>> repositoryMap;
+  private final Map<Class<?>, EntityRepository<?>> repositoryMap;
 
   /**
    * The constructor.
    */
+  @SuppressWarnings({ "unchecked", "rawtypes" })
   public EntityRepositoryManagerImpl() {
 
     super();
     this.repositoryMap = new HashMap<>();
-    ServiceLoader<EntityRepository> serviceLoader = ServiceLoader.load(EntityRepository.class);
-    for (EntityRepository<?> repository : serviceLoader) {
-      registerRepository(repository);
-    }
+    ServiceLoader<EntityRepository<?>> serviceLoader = (ServiceLoader) ServiceLoader.load(EntityRepository.class);
+    ServiceHelper.all(serviceLoader, this.repositoryMap, EntityRepositoryManagerImpl::getEntityClass);
   }
 
-  /**
-   * @param repository the {@link EntityRepository} to register.
-   */
-  private void registerRepository(EntityRepository<?> repository) {
+  private static Class<?> getEntityClass(EntityRepository<?> repository) {
 
     Class<?> entityClass = repository.getEntityClass();
     if (entityClass == null) {
       throw new ObjectNotFoundException("EntityClass", repository);
     }
-    this.repositoryMap.put(entityClass, repository);
+    return entityClass;
   }
 
-  @SuppressWarnings("unchecked")
+  @SuppressWarnings({ "unchecked", "rawtypes" })
   @Override
   public <E extends Entity> EntityRepository<E> getRepository(Class<E> entityClass) {
 
