40c06156ca0e65235d1201661b0f27bc64756515
Jörg Hohwiller <joerg.hohwiller@capgemini.com>
2024-06-26 23:52:34 +0200

    improved tests

--- bean/src/test/java/io/github/mmm/entity/property/link/LinkPropertyTest.java
+++ bean/src/test/java/io/github/mmm/entity/property/link/LinkPropertyTest.java
@@ -2,20 +2,41 @@ package io.github.mmm.entity.property.link;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.time.Instant;
+import java.util.UUID;
+
 import org.junit.jupiter.api.Test;
 
 import io.github.mmm.base.exception.ReadOnlyException;
 import io.github.mmm.bean.BeanFactory;
 import io.github.mmm.entity.bean.PropertyTest;
 import io.github.mmm.entity.id.Id;
+import io.github.mmm.entity.id.LongInstantId;
+import io.github.mmm.entity.id.LongRevisionlessId;
 import io.github.mmm.entity.id.LongVersionId;
+import io.github.mmm.entity.id.StringInstantId;
+import io.github.mmm.entity.id.StringRevisionlessId;
+import io.github.mmm.entity.id.StringVersionId;
+import io.github.mmm.entity.id.UuidInstantId;
+import io.github.mmm.entity.id.UuidRevisionlessId;
+import io.github.mmm.entity.id.UuidVersionId;
 import io.github.mmm.entity.link.Link;
+import io.github.mmm.marshall.MarshallingConfig;
+import io.github.mmm.marshall.StandardFormat;
+import io.github.mmm.marshall.StructuredReader;
+import io.github.mmm.marshall.StructuredTextFormat;
+import io.github.mmm.marshall.StructuredWriter;
 import io.github.mmm.property.WritableProperty;
 
 /**
  * Test of {@link LinkProperty}.
  */
 public class LinkPropertyTest extends PropertyTest<Link<Target>, LinkProperty<Target>> {
+
+  /**
+   *
+   */
+  private static final String INSTANT = "1999-12-31T23:59:59.123456789Z";
 
   private static final Id<Target> ID;
 
@@ -42,7 +63,7 @@ public class LinkPropertyTest extends PropertyTest<Link<Target>, LinkProperty<Ta
 
     // arrange
     LinkProperty<Target> linkProperty = new LinkProperty<>("target", Target.class, null);
-    LongVersionId<Target> id2 = new LongVersionId<>(Target.class, 4712L, 1L);
+    LongVersionId<Target> id = new LongVersionId<>(Target.class, 4712L, 1L);
     // act
     LinkProperty<Target> readOnlyLinkProperty = WritableProperty.getReadOnly(linkProperty);
     // assert
@@ -50,9 +71,50 @@ public class LinkPropertyTest extends PropertyTest<Link<Target>, LinkProperty<Ta
     linkProperty.set(LINK);
     Link<Target> readOnlyLink = readOnlyLinkProperty.get();
     assertThrows(ReadOnlyException.class, () -> {
-      readOnlyLink.getTarget().setId(id2);
+      readOnlyLink.getTarget().setId(id);
     });
     assertThat(readOnlyLink).isNotNull().isNotSameAs(LINK);
+  }
+
+  @Test
+  public void testMapJson() {
+
+    Instant instant = Instant.parse(INSTANT);
+    // LongId variants
+    checkMapJson("4711", 4711L, null, LongRevisionlessId.class);
+    checkMapJson("{\"l\":4711,\"v\":42}", 4711L, 42L, LongVersionId.class);
+    checkMapJson("{\"l\":4711,\"t\":\"" + INSTANT + "\"}", 4711L, instant, LongInstantId.class);
+    // UuidId variants
+    UUID uuid = UUID.randomUUID();
+    String uuidString = uuid.toString();
+    checkMapJson("\"" + uuidString + "\"", uuid, null, UuidRevisionlessId.class);
+    checkMapJson("{\"u\":\"" + uuidString + "\",\"v\":43}", uuid, 43L, UuidVersionId.class);
+    checkMapJson("{\"u\":\"" + uuidString + "\",\"t\":\"" + INSTANT + "\"}", uuid, instant, UuidInstantId.class);
+    // String variants
+    String pk = "primary-key";
+    checkMapJson("\"" + pk + "\"", pk, null, StringRevisionlessId.class);
+    checkMapJson("{\"s\":\"" + pk + "\",\"v\":44}", pk, 44L, StringVersionId.class);
+    checkMapJson("{\"s\":\"" + pk + "\",\"t\":\"" + INSTANT + "\"}", pk, instant, StringInstantId.class);
+  }
+
+  private void checkMapJson(String json, Object pk, Object revision, Class<? extends Id> idClass) {
+
+    // arrange
+    LinkProperty<Target> linkProperty = new LinkProperty<>("target", Target.class, null);
+    StructuredTextFormat jsonFormat = StandardFormat.json(MarshallingConfig.NO_INDENTATION);
+    StructuredReader reader = jsonFormat.reader(json);
+    StringBuilder sb = new StringBuilder(json.length());
+    StructuredWriter writer = jsonFormat.writer(sb);
+    // act
+    linkProperty.read(reader);
+    Id<Target> id = linkProperty.get().getId();
+    linkProperty.write(writer);
+    // assert
+    assertThat(id.get()).isEqualTo(pk);
+    assertThat(id.getRevision()).isEqualTo(revision);
+    assertThat(id.getEntityClass()).isSameAs(Target.class);
+    assertThat(id.getClass()).isSameAs(idClass);
+    assertThat(sb.toString()).isEqualTo(json);
   }
 
   @Override
