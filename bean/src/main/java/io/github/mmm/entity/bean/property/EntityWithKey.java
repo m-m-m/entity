99ffd83c0270ce88ce815fca83a3142a482761cf
Jörg Hohwiller <hohwille@users.sourceforge.net>
2025-12-22 14:38:25 +0100

    added EntityWithKey

---
+++ bean/src/main/java/io/github/mmm/entity/bean/property/EntityWithKey.java
@@ -0,0 +1,20 @@
+package io.github.mmm.entity.bean.property;
+
+import io.github.mmm.base.metainfo.MetaInfos;
+import io.github.mmm.bean.AbstractInterface;
+import io.github.mmm.entity.bean.EntityBean;
+import io.github.mmm.property.string.StringProperty;
+
+/**
+ * Interface for an {@link EntityBean} having a {@link #Key()}.
+ */
+@AbstractInterface
+public abstract interface EntityWithKey extends EntityBean {
+
+  /**
+   * @return the (business) key of this entity. Typically its value should be unique per entity.
+   */
+  @MetaInfos("score=1")
+  StringProperty Key();
+
+}
